e13544ee6ce8cbd1df2a18de7dbdd150b44f63a9
Noella Marie Chartine Uwase <141454615+Chartine02@users.noreply.github.com>
2024-10-05 19:50:20 +0100

    fix: deploy web static

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -3,7 +3,7 @@
 from fabric.api import env, put, run
 from os.path import exists
 
-env.hosts = ["54.209.26.141", "18.215.182.32"]
+env.hosts = ["100.27.232.208", "3.87.158.125"]
 env.user = "ubuntu"
 env.key = "~/.ssh/id_rsa"
 
